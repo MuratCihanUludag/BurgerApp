cf849a0c6f5f8fafd1c07900dec4d073e240e56f
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-19 23:16:09 +0300

    Marge Fixed

--- BurgerApp/BurgerApp.DAL/Entities/Concrate/MenuClasses/Drink.cs
+++ BurgerApp/BurgerApp.DAL/Entities/Concrate/MenuClasses/Drink.cs
@@ -1,5 +1,5 @@
 
-﻿using BurgerApp.DAL.Comman;
+using BurgerApp.DAL.Comman;
 using BurgerApp.DAL.Entities.Abstract.Base;
 using BurgerApp.DAL.Entities.Abstract.MenuClasses;
 using BurgerApp.DAL.Entities.Concrate.OtherClasses;
@@ -13,23 +13,12 @@ using System.Threading.Tasks;
 
 namespace BurgerApp.DAL.Entities.Concrate.MenuClasses
 {
-<<<<<<< HEAD
-    public class Drink : BaseImage, IDrink
-=======
 
-    public class Drink : BaseImage
->>>>>>> c2f32e47f727f5a400a8faeafa35b234d5c90b48
+    public class Drink : BaseImage, IDrink
     {
         public string Name { get; set; }
         public ProductSize Size { get; set; }
         public double Price { get; set; }
         public ICollection<Menu> Menus { get; set; }
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> c2f32e47f727f5a400a8faeafa35b234d5c90b48
     }
 }
