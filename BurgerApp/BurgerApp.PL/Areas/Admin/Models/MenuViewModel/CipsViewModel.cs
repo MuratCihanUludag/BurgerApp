3414dd9aea0f0aa38eb28beaf3c7ccba9ee5f2ff
BurakAKYIL <mburakakyil@gmail.com>
2024-03-21 17:33:17 +0300

    CipsController eklemeler yapıldı

---
+++ BurgerApp/BurgerApp.PL/Areas/Admin/Models/MenuViewModel/CipsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BurgerApp.BLL.ViewModels.General_Models;
+using BurgerApp.DAL.Comman;
+using System.ComponentModel.DataAnnotations;
+
+namespace BurgerApp.PL.Areas.Admin.Models.MenuViewModel
+{
+    public class CipsViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public ProductSize Size { get; set; }
+        [Required]
+        public IFormFile Image { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public ICollection<MenuDTO> Menus { get; set; }
+    }
+}
