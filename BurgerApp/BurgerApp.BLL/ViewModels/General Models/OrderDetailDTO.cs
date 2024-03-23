949652eb9d0bf60c7db3413fa5a408c1844c4c2a
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-23 17:10:24 +0300

    Order Dto silindi, MenuDto daki claslar degistirildi, OrderDetaila nullable alabilir ozelligi eklendi

--- BurgerApp/BurgerApp.BLL/ViewModels/General Models/OrderDetailDTO.cs
+++ BurgerApp/BurgerApp.BLL/ViewModels/General Models/OrderDetailDTO.cs
@@ -10,22 +10,25 @@ using BurgerApp.BLL.ViewModels.Other_Models;
 using BurgerApp.DAL.Entities.Concrate.MenuClasses;
 using BurgerApp.BLL.ViewModels.Menu_Models;
 using BurgerApp.DAL.Entities.Abstract.OtherClasses;
+using BurgerApp.PL.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BurgerApp.BLL.ViewModels.General_Models
 {
     public class OrderDetailDTO : BaseDTO
     {
+        public string UserId { get; set; }
+        public BurgerAppUser User { get; set; }
         public int BurgerId { get; set; }
-        public Burger Burger { get; set; }
+        public Burger? Burger { get; set; }
         public int DrinkId { get; set; }
-        public Drink Drink { get; set; }
+        public Drink? Drink { get; set; }
         public int CipsId { get; set; }
-        public Cips Cips { get; set; }
+        public Cips? Cips { get; set; }
         public int Count { get; set; }
-        public ICollection<Sauce> Sauces { get; set; }
-        public ICollection<ExtraMaterial> ExtraMetarials { get; set; }
-        public int OrderId { get; set; }
-        public Order Order { get; set; }
+        public ICollection<Sauce>? Sauces { get; set; }
+        public ICollection<ExtraMaterial>? ExtraMetarials { get; set; }
+        public bool IsSell { get; set; }
         public double OrderDetailTotalPrice()
         {
             double totalSaucePrice = Sauces.Sum(sauce => sauce.Price);
